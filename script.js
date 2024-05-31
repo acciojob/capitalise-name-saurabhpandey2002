87ca2e7734e2a7fccc70ce5cf81810b5d8e80c2e
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-05-31 18:39:15 +0530

    ADD

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let input = document.getElementById("fname");
+
+input.addEventListener("blur" ,Onblur);
+function Onblur() {
+	let capName=input.value;
+	input.value=capName.toUpperCase()
+}
